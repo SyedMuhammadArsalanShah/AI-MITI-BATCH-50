0f04ae04759b1d9a4b5f40e3e1f3db454b20d1c3
SyedMuhammadArsalanShah <smarsalanshah125@gmail.com>
2026-07-08 20:42:43 +0500

    Lecture 01 Varaibles

---
+++ Lecture01Variables/main.py
@@ -0,0 +1,20 @@
+print("Bismillah")
+
+
+print("Welcom to MITI")
+
+
+name="Haseeb"
+age="26"
+phone="15" 
+courseName="AI"
+
+course_Timing="7 to 9"
+
+
+
+print("Student Name ",name)
+print("Student Age ",age)
+print("Student Phone ",phone)
+print("Student Course Timing",course_Timing)
+print("Student Course Name",courseName)
