10b3cc241c513cbfb49c35f71f5abed4c1366939
SyedMuhammadArsalanShah <smarsalanshah125@gmail.com>
2026-07-17 20:44:09 +0500

    Code

---
+++ Lecture06List/main.py
@@ -0,0 +1,24 @@
+
+
+students_name =["HAseeb", "Jawad", "Ali", "Moiz"]
+
+
+
+
+
+
+
+print(students_name)
+
+
+students_name.insert(1,"Hasan")
+
+print(students_name)
+
+
+
+
+
+for i in students_name:
+
+    print("Student Of Year",i)
---
+++ Lecture07Dictionary/main.py
@@ -0,0 +1,10 @@
+info={
+"name":"Haseeb",
+"Course":50,
+"Age":23,
+"edu":{"BS":2026, "Inter":2020}
+}
+
+
+print(info)
+print(info["edu"]["BS"])
